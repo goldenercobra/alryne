429858ced1247c7477cffa4a166c7e5b3b5df7c2
Abhi Agarwal <goldenercobra@gmail.com>
2018-03-01 10:39:08 -0500

    refactor(common/models): use inheritance for Caucus
    - adds abstract Caucus type with common fields
    - removes json_serializable and does serialization manually
    - adds a "tag" enum so Json can be properly coverted

--- alryne/lib/src/models/caucus.dart
+++ alryne/lib/src/models/caucus.dart
@@ -1,64 +1,150 @@
-import 'package:json_annotation/json_annotation.dart' show JsonSerializable;
 import 'package:meta/meta.dart' show required;
 
 import 'delegate.dart' show Delegate;
 
-part 'caucus.g.dart';
+enum _CaucusTag { caucus, moderated, unmoderated, other }
 
-/// Type of caucus, moderated or unmoderated
-enum CaucusType {
-  /// moderated caucus
-  moderated,
+/// Base Caucus Implementation
+abstract class Caucus extends Object {
+  /// Topic of the caucus
+  final String topic;
 
-  /// unmoderated caucus
-  unmoderated,
-
-  /// extension to a previous caucus
-  extension,
-
-  /// other (censure, extraneous votes)
-  other
-}
-
-/// Describes a caucus
-@JsonSerializable()
-class Caucus extends Object with _$CaucusSerializerMixin {
-  /// Length of caucus (in seconds)
-  @override
+  /// Length of the caucus (in seconds)
   final int length;
 
-  /// Length of speaking time (in seconds), only for moderated caucus
-  @override
-  int speakingLength;
+  static const _CaucusTag _tag = _CaucusTag.caucus;
 
-  /// Type of caucus
-  @override
-  final CaucusType type;
+  /// Constructs [Caucus]
+  Caucus({@required this.topic, @required this.length})
+      : assert(topic != null),
+        assert(length != null);
 
-  /// List of speakers, only for moderated caucus
-  @override
-  List<Delegate> speakers;
-
-  /// Constructs Caucus from [length], [type], and optional [speakingLength]
-  Caucus({@required this.length, @required this.type, int speakingLength})
-      : assert(length != null),
-        assert(type != null) {
-    if (type != CaucusType.moderated) {
-      // Unmoderated caucuses do not need a speaking time
-      return;
-    }
-    if (length % speakingLength == 0) {
-      speakers = new List<Delegate>.filled((length / speakingLength).round(), null);
-      this.speakingLength = speakingLength;
-    } else {
-      throw new ArgumentError(
-          '[speakingLength] must divide evenly into caucus length');
+  /// Constructs [Caucus] indirectly by delegating to child objects.
+  factory Caucus.fromJson(Map<String, dynamic> map) {
+    switch (_CaucusTag.values.firstWhere(
+        (_CaucusTag tag) => tag.toString().split('.')[1] == map['tag'],
+        orElse: () =>
+            throw ArgumentError.value(map, 'map', 'invalid [tag] provided'))) {
+      case _CaucusTag.moderated:
+        return new ModeratedCaucus.fromJson(map);
+      case _CaucusTag.unmoderated:
+        return new UnmoderatedCaucus.fromJson(map);
+      case _CaucusTag.other:
+        return new OtherCaucus.fromJson(map);
+      case _CaucusTag.caucus:
+      default:
+        throw new ArgumentError.value(map, 'map', 'invalid [tag] provided');
     }
   }
 
-  /// Constructs [Caucus] from [map]
-  factory Caucus.fromJson(Map<String, dynamic> map) => _$CaucusFromJson(map);
+  /// Convert [Caucus] into a Map Object which can be serialized
+  Map<String, dynamic> toJson() => <String, dynamic>{
+        'tag': _tag.toString().split('.')[1],
+        'topic': topic,
+        'length': length,
+      };
+}
 
-  /// Return maximum speakers
-  int get speakersSize => speakers.length;
+/// Moderated Caucus, a set list of speakers and speaking time
+class ModeratedCaucus extends Caucus {
+  /// How long each speaker can speak (in seconds)
+  int speakingLength;
+
+  /// List of speakers, in order
+  List<Delegate> _speakers;
+
+  static const _CaucusTag _tag = _CaucusTag.moderated;
+
+  /// Constructs [ModeratedCaucus]
+  ModeratedCaucus({
+    @required String topic,
+    @required int length,
+    @required this.speakingLength,
+  })  : assert(topic != null),
+        assert(length != null),
+        assert(speakingLength != null),
+        super(topic: topic, length: length) {
+    if (length % speakingLength != 0) {
+      throw new ArgumentError.value(speakingLength, 'speakingLength',
+          'must divide into [length] perfectly');
+    }
+  }
+
+  /// Construct [ModeratedCaucus] from [map]
+  @override
+  ModeratedCaucus.fromJson(Map<String, dynamic> map)
+      : assert(map['tag'] != null),
+        assert(map['tag'] == _tag.toString().split('.')[1]),
+        assert(map['speakingLength'] != null),
+        speakingLength = map['speakingLength'],
+        assert(map['speakers'] != null),
+        _speakers = map['speakers']
+            .map((Map<String, dynamic> item) => new Delegate.fromJson(map)),
+        assert(map['topic'] != null),
+        assert(map['length'] != null),
+        super(topic: map['topic'], length: map['length']);
+
+  /// Convert [ModeratedCaucus] into a Map Object which can be serialized
+  /// Inherits from [Caucus] and adds additional fields
+  @override
+  Map<String, dynamic> toJson() => super.toJson()
+    ..addAll(<String, dynamic>{
+      'speakingLength': speakingLength,
+      'speakers': _speakers
+    });
+
+  /// Number of speakers avaliable
+  int get speakersSize => (super.length / speakingLength).round();
+
+  /// Returns an encapsulated speakers list
+  List<Delegate> get speakers => _speakers;
+
+  /// Set [speakers], checking that the length of the given List matches
+  void set speakers(List<Delegate> speakers) {
+    if (speakers.length != speakersSize) {
+      throw new ArgumentError.value(speakers, 'speakers',
+          'speakers length must match with determined length');
+    }
+    _speakers = speakers;
+  }
+}
+
+/// Unmoderated Caucus, basically a free-for-all
+class UnmoderatedCaucus extends Caucus {
+  static const _CaucusTag _tag = _CaucusTag.unmoderated;
+
+  /// Constructs [UnmoderatedCaucus]
+  UnmoderatedCaucus({@required String topic, @required int length})
+      : assert(topic != null),
+        assert(length != null),
+        super(topic: topic, length: length);
+
+  /// Constructs [UnmoderatedCaucus] from [map]
+  @override
+  UnmoderatedCaucus.fromJson(Map<String, dynamic> map)
+      : assert(map['tag'] != null),
+        assert(map['tag'] == _tag.toString().split('.')[1]),
+        assert(map['topic'] != null),
+        assert(map['length'] != null),
+        super(topic: map['topic'], length: map['length']);
+}
+
+/// Other Caucus, for unrelated events (censure, join new country, etc)
+class OtherCaucus extends Caucus {
+  static const _CaucusTag _tag = _CaucusTag.other;
+
+  /// Constructs [OtherCaucus]
+  OtherCaucus({@required String topic, @required int length})
+      : assert(topic != null),
+        assert(length != null),
+        super(topic: topic, length: length);
+
+  /// Constructs [OtherCaucus] from [map]
+  @override
+  OtherCaucus.fromJson(Map<String, dynamic> map)
+      : assert(map['tag'] != null),
+        assert(map['tag'] == _tag.toString().split('.')[1]),
+        assert(map['topic'] != null),
+        assert(map['length'] != null),
+        super(topic: map['topic'], length: map['length']);
 }

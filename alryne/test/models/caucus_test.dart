c945360a4c8eb9974d806c3d8f574f82fab6ae85
Abhi Agarwal <goldenercobra@gmail.com>
2018-03-09 13:57:57 -0500

    feat(common/test): test speakers setter acts properly
    - throws if incorrect size, returnsNormally if correct

--- alryne/test/models/caucus_test.dart
+++ alryne/test/models/caucus_test.dart
@@ -13,7 +13,7 @@ void main() {
       expect(caucus.speakingLength, equals(10));
       expect(caucus.speakersSize, equals(10));
 
-      expect(new ModeratedCaucus(topic: 'test2', length: 100, speakingLength: 75), throwsArgumentError);
+      expect(() => new ModeratedCaucus(topic: 'test2', length: 100, speakingLength: 75), throwsArgumentError);
     });
     test('unmoderated', () {
       final UnmoderatedCaucus caucus = new UnmoderatedCaucus(topic: 'test', length: 600);
@@ -50,6 +50,41 @@ void main() {
       /// Json has to be compared since classes data cannot be compared
       expect(constructedCaucus, const isInstanceOf<OtherCaucus>());
       expect(constructedCaucus.toJson(), equals(caucus.toJson()));
+    });
+  });
+
+  group('moderated caucus speakers size', (){
+    test('nothing happens if correct size', (){
+      final ModeratedCaucus caucus = new ModeratedCaucus(topic: 'test', length: 150, speakingLength: 50);
+      final List<Delegate> speakers = <Delegate>[
+        new Delegate(
+          names: <String>['A', 'B', 'C'],
+          country: new Country(name: 'Abcistan', short: 'ABC', id: 123),
+        ),
+        new Delegate(
+          names: <String>['D', 'E', 'F'],
+          country: new Country(name: 'Defistan', short: 'DEF', id: 456),
+        ),
+        new Delegate(
+          names: <String>['G', 'H', 'I'],
+          country: new Country(name: 'GHIistan', short: 'GHI', id: 789),
+        ),
+      ];
+      expect(() => caucus.speakers = speakers, returnsNormally);
+    });
+    test('throws if incorrect size', (){
+      final ModeratedCaucus caucus = new ModeratedCaucus(topic: 'test', length: 150, speakingLength: 50);
+      final List<Delegate> speakers = <Delegate>[
+        new Delegate(
+          names: <String>['A', 'B', 'C'],
+          country: new Country(name: 'Abcistan', short: 'ABC', id: 123),
+        ),
+        new Delegate(
+          names: <String>['D', 'E', 'F'],
+          country: new Country(name: 'Defistan', short: 'DEF', id: 456),
+        ),
+      ];
+      expect(() => caucus.speakers = speakers, throwsArgumentError);
     });
   });
 }

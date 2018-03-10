d740b0f01367561a508e7d98ba92f99402493954
Abhi Agarwal <goldenercobra@gmail.com>
2018-03-10 14:02:00 -0500

    doc(common/models): add missing documentation of Json constructor

--- alryne/lib/src/models/crisis.dart
+++ alryne/lib/src/models/crisis.dart
@@ -7,10 +7,12 @@ part 'crisis.g.dart';
 /// A crisis, a change in events elapsed
 class Crisis extends Object with _$CrisisSerializerMixin {
   /// Topic of Crisis
+  @override
   final String topic;
 
   /// Construct [Crisis] given [topic]
   Crisis({@required this.topic});
 
+  /// Construct [Crisis] from [map]
   factory Crisis.fromJson(Map<String, dynamic> map) => _$CrisisFromJson(map);
 }

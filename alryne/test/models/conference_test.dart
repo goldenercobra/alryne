22b24dfc97ff256c1febcd125a11f55826b50617
Abhi Agarwal <goldenercobra@gmail.com>
2018-03-10 16:57:29 -0500

    test(common): add Conference test
    - tests generator to ensure it's building valid results

---
+++ alryne/test/models/conference_test.dart
@@ -0,0 +1,50 @@
+import 'dart:convert' show json;
+import 'dart:math' as math;
+
+import 'package:alryne/models.dart' show Conference, Committee;
+
+import 'package:test/test.dart';
+
+void main() {
+  group('constructor', () {
+    test('> direct constructor', () {
+      expect(
+          () => new Conference(
+              id: 'ABCD', committee: new Committee(topic: 'EFGI')),
+          returnsNormally);
+      final Conference conference =
+          new Conference(id: 'ABCD', committee: new Committee(topic: 'EFGH'));
+      expect(conference.id, equals('ABCD'));
+      expect(conference.committee, equals(new Committee(topic: 'EFGH')));
+    });
+    test('> create constructor', () {
+      expect(() => new Conference.create(topic: 'ABCD'), returnsNormally);
+      final Conference conference = new Conference.create(topic: 'ABCD');
+      for (int codeUnit in conference.id.codeUnits) {
+        expect(codeUnit, inInclusiveRange(65, 65 + 25));
+      }
+      expect(conference.committee, equals(new Committee(topic: 'ABCD')));
+    });
+  });
+
+  group('serialization', () {
+    test('> serializes correctly', () {
+      final Conference conference =
+          new Conference(id: 'ABCD', committee: new Committee(topic: 'EFGH'));
+      final Conference constructedConference =
+          new Conference.fromJson(json.decode(json.encode(conference)));
+      expect(constructedConference, equals(conference));
+    });
+  });
+
+  group('generator', () {
+    for (int i = 1; i <= 25; i++) {
+      test('> random generator (test $i)', () {
+        final String id = Conference.generateId();
+        for (int codeUnit in id.codeUnits) {
+          expect(codeUnit, inInclusiveRange(65, 65 + 25));
+        }
+      });
+    }
+  });
+}

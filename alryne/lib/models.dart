09da3e638e5dbc1b01f4a4348e6ba981373745be
Abhi Agarwal <goldenercobra@gmail.com>
2018-03-10 13:59:54 -0500

    fix(common/models): export crisis.dart

--- alryne/lib/models.dart
+++ alryne/lib/models.dart
@@ -4,4 +4,5 @@ export 'src/models/caucus.dart';
 export 'src/models/committee.dart';
 export 'src/models/conference.dart';
 export 'src/models/country.dart';
+export 'src/models/crisis.dart';
 export 'src/models/delegate.dart';
